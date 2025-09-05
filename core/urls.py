8df95c0eec841780a79fdea2a742d4c336cbb605
Mahmoud Said <mahmoudsaid1704@gmail.com>
2025-09-05 03:37:54 +0300

    Sprint4 Finished - Add Groups/Leaderboard

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -7,3 +10,6 @@ urlpatterns = [
     path('api/prayers/', include('prayers.urls')),
     path('api/groups/', include('groups.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
